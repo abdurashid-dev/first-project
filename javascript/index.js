26de4bed7a50bc79ef21be88f56b4779e0ac800a
Abdurashid-dev <abdurashiddev@gmail.com>
2021-02-07 16:14:54 +0500

    Cloth project

---
+++ javascript/index.js
@@ -0,0 +1,381 @@
+let n = 'Women Cloth';
+let acc = 'Accessories';
+let c = 'Cotton';
+let data = [
+    {
+        img: "menpic9.jpg",
+        title: "T-short",
+        rate: 5,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2020,
+        description: "Cotton"
+    },
+    {
+        img: "menpic10.jpg",
+        title: "T-short",
+        rate: 4,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2018,
+        description: "Cotton"
+    },
+    {
+        img: "menpic11.jpg",
+        title: "T-short",
+        rate: 4.5,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2018,
+        description: "Cotton"
+    },
+    {
+        img: "menpic12.jpg",
+        title: "T-short",
+        rate: 4,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2018,
+        description: "Cotton"
+    },
+    {
+        img: "menpic12.jpg",
+        title: "T-short",
+        rate: 4,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2018,
+        description: "Cotton"
+    },
+    {
+        img: "menpic12.jpg",
+        title: "T-short",
+        rate: 4,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2018,
+        description: "Cotton"
+    },
+    {
+        img: "menpic12.jpg",
+        title: "T-short",
+        rate: 4,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2018,
+        description: "Cotton"
+    },
+    {
+        img: "menpic1.jpg",
+        title: "Sweatshirt",
+        rate: 4.5,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2020,
+        description: "Cotton"
+    },
+    {
+        img: "menpic2.jpg",
+        title: "Sweatshirt",
+        rate: 4.5,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2019,
+        description: "Cotton"
+    },
+    {
+        img: "menpic3.jpg",
+        title: "Sweatshirt",
+        rate: 2.5,
+        watch: Math.floor(Math.random()*1000),
+        year: 2016,
+        price: Math.floor(Math.random()*1000),
+        description: "Cotton"
+    },
+    {
+        img: "menpic4.jpg",
+        title: "Sweatshirt",
+        rate: 4,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2018,
+        description: "Cotton"
+    },
+    {
+        img: "menpic5.jpg",
+        title: "Sweatshirt",
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2019,
+        description: "Cotton"
+    },
+
+    {
+        img: "menpic6.jpg",
+        title: "Sweatshirt",
+        rate: 3.9,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2019,
+        description: "Cotton"
+    },
+    {
+        img: "menpic8.jpg",
+        title: "Sweatshirt",
+        rate: 4.3,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2020,
+        description: "Cotton"
+    },
+    {
+        img: "womenpic.jpg",
+        title: "Women Clothe",
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2019,
+        description: "Cotton"
+    },
+    {
+        img: "womenpic3.jpg",
+        title: "Women Clothe",
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2019,
+        description: "Cotton"
+    },
+    {
+        img: "womenpic2.jpg",
+        title: "Women Clothe",
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2020,
+        description: "Cotton"
+    },
+    {
+        img: "womenpic4.jpg",
+        title: "Women Clothe",
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: "Cotton"
+    },
+    {
+        img: "womenpic5.jpg",
+        title: "Women Clothe",
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: "Cotton"
+    },
+    {
+        img: "womenpic6.jpg",
+        title: "Women Clothe",
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: "Cotton"
+    },
+    {
+        img: "womenpic7.jpg",
+        title: "Women Clothe",
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: "Cotton"
+    },
+    {
+        img: "womenpic8.jpg",
+        title: n,
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: "Cotton"
+    },
+    {
+        img: "womenpic9.jpg",
+        title: n,
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: "Cotton"
+    },
+    {
+        img: "womenpic.jpg",
+        title: "Women Clothe",
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: "Cotton"
+    },
+    {
+        img: "acc1.jpg",
+        title: acc,
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: 'Polyester'
+    },
+    {
+        img: "acc2.jpg",
+        title:  'Accessories',
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: 'Polyester'
+    },
+    {
+        img: "acc3.jpg",
+        title:  'Accessories',
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: 'Polyester'
+    },
+    {
+        img: "acc4.jpg",
+        title: 'Accessories',
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: 'Polyester'
+    },
+    {
+        img: "acc10.jpg",
+        title: 'Accessories',
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: 'Polyester'
+    },
+    {
+        img: "acc6.jpg",
+        title: 'Caps',
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: 'Polyester'
+    },
+    {
+        img: "acc7.jpg",
+        title: 'Caps',
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: 'Cotton'
+    },
+    {
+        img: "acc8.jpg",
+        title: 'Accessories',
+        rate: 3.8,
+        watch: Math.floor(Math.random()*1000),
+        price: Math.floor(Math.random()*1000),
+        year: 2017,
+        description: 'Cotton'
+    },
+    // {
+    //     img: "acc9.jpg",
+    //     title: 'Accessories',
+    //     rate: 3.8,
+    //     watch: Math.floor(Math.random()*1000),
+    //     price: Math.floor(Math.random()*1000),
+    //     year: 2017,
+    //     description: 'Cotton'
+    // },
+
+
+
+];
+$(document).ready(function(){
+    buildProduct(data);
+})
+function buildProduct(product){
+    let full = product.map((elem)=>{
+        let product =       
+          '<div class="col" id="col" style="height:550px"data-aos="fade-up" data-aos-duration="700">'+
+          '<div class="card" id="respon">'+
+            '<div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">'+
+              '<img src="cloth/'+elem.img+'" class="img-fluid" />'+
+              '<a href="#!">'+
+                '<div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>'+
+              '</a>'+
+            '</div>'+
+            '<div class="card-body">'+
+              '<h5 class="card-title">'+elem.title+'</h5>'+
+              '<p class="card-text">'+
+               'Prise:'+elem.price+'</p>'+
+               '<p>Year:2021</p>'+
+               '<p>From'+elem.description+'</p>'+
+               '<p><span style="color: orange;">'+
+                '<i class="fas fa-star"></i>'+
+                '<i class="fas fa-star"></i>'+
+                '<i class="fas fa-star"></i>'+
+                '<i class="fas fa-star"></i>'+
+                '<i class="fas fa-star-half-alt"></i>'+
+              '</span>'+elem.rate+'</p>'+
+            '<p><span><i class="fas fa-eye"></i></span>'+elem.watch+'</p>'+
+            '<button class="bt">SHOP NOW</button>'+
+            // '<button class="bty"><i class="fas fa-heart"></i></button>'+
+            '</div>'+
+          '</div>'+
+        '</div>'
+      return product;
+    }).join('');
+    $('#product').empty();
+    $('#product').append(full);
+}
+
+$('.low').click(function () { 
+    let lowToHigh = data.sort(function(a,b){
+    return a.price-b.price
+ });
+ buildProduct(lowToHigh);
+});
+
+
+
+$('.high').click(function(){
+    let highToLow = data.sort((a,b)=> b.price-a.price)
+    buildProduct(highToLow);
+});
+
+
+$('.brands').click(function(){
+    let name = this.innerHTML
+    brand(name);
+})
+
+function brand(title){
+    let brandArr = [];
+    if(title == 'All'){
+        brandArr = data
+    } else{
+        brandArr = data.filter((elem)=>{
+            if(elem.title.includes(title)){
+                return true
+            } else{
+                return false
+            }
+        })
+    }
+    buildProduct(brandArr);
+}
+
